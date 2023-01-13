6c11c6870927b08c4f868316a9e4e96f3200ead9
94andradeflavio <94andrade.flavio@gmail.com>
2023-01-13 09:15:55 -0300

    Implementado TabView

--- src/components/Sections-Components/TabView/index.jsx
+++ src/components/Sections-Components/TabView/index.jsx
@@ -1,25 +1,74 @@
-import React from 'react'
+import { useState, useRef } from 'react'
 import './styles.css'
 
+const tabData = [
+    {
+        tab: 'destaques',
+        title: 'lorem ipsum',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sunt explicabo amet laborum, cupiditate non mollitia ipsum voluptates repudiandae rerum fuga doloremque maiores assumenda nostrum reiciendis molestias nihil neque iure.'
+    },
+    {
+        tab: 'inovação',
+        title: 'ajfaspkj',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sunt explicabo amet laborum, cupiditate non mollitia ipsum voluptates repudiandae rerum fuga doloremque maiores assumenda nostrum reiciendis molestias nihil neque iure.'
+    },
+    {
+        tab: 'produtos',
+        title: 'lorem ipsuadsffam',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sunt explicabo amet laborum, cupiditate non mollitia ipsum voluptates repudiandae rerum fuga doloremque maiores assumenda nostrum reiciendis molestias nihil neque iure.'
+    },
+    {
+        tab: 'impressões',
+        title: 'lorsfjasem ipsum',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sunt explicabo amet laborum, cupiditate non mollitia ipsum voluptates repudiandae rerum fuga doloremque maiores assumenda nostrum reiciendis molestias nihil neque iure.'
+    },
+    {
+        tab: 'estrelas',
+        title: 'lorem 8888ipsum',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sunt explicabo amet laborum, cupiditate non mollitia ipsum voluptates repudiandae rerum fuga doloremque maiores assumenda nostrum reiciendis molestias nihil neque iure.'
+    },
+    {
+        tab: 'confiança',
+        title: 'lorem pspsps',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sunt explicabo amet laborum, cupiditate non mollitia ipsum voluptates repudiandae rerum fuga doloremque maiores assumenda nostrum reiciendis molestias nihil neque iure.'
+    },
+]
+
 const TabView = () => {
+    const lineRef = useRef()
+    const [active, setActive] = useState(0)
+
+    const handleClick = index => {
+        setActive(index)
+        handleLine(index)
+    }
+
+    const handleLine = i => {
+        const selectedReference = document.querySelectorAll('.tab-view li')[i]
+        const line = lineRef.current
+
+        line.style.top = `${selectedReference.offsetTop}px`
+        line.style.height = `${selectedReference.offsetHeight}px`
+        line.style.backgroundColor = 'red'
+    }
+
     return (
         <div className="tab-view">
             <div className="line-area">
                 <div className="line">
-                    <div className="inner-line" />
+                    <div className="inner-line" ref={ lineRef } />
                 </div>
                 <ul>
-                    <li>Destaques</li>
-                    <li>Inovação</li>
-                    <li>Produtos</li>
-                    <li>Impressões</li>
-                    <li>Estrelas</li>
-                    <li>Confiança</li>
+                    { tabData.map((tab, i) => {
+                        return (
+                            <li key={i} className={ i === active ? 'active' : '' } onClick={_ => handleClick(i)} >{ tab.tab }</li>
+                        )
+                    }) }
                 </ul>
             </div>
             <div className="content">
-                <h4 className="title">Lorem ipsum</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sunt explicabo amet laborum, cupiditate non mollitia ipsum voluptates repudiandae rerum fuga doloremque maiores assumenda nostrum reiciendis molestias nihil neque iure.</p>
+                <h4 className="title">{ tabData[active].title }</h4>
+                <p>{ tabData[active].description }</p>
             </div>
         </div>
     )
